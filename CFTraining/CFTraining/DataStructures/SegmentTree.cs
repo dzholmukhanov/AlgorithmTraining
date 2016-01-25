7e593df16d426f3943552f9e7d2d8b0de9007a64
darkhan.zholmukhanov <dzholmukhanov@gmail.com>
2016-01-25 17:47:46 +0600

    Added segment tree implemetation. Not a generic one. Finds the minimum in a range.

---
+++ CFTraining/CFTraining/DataStructures/SegmentTree.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFTraining.DataStructures
+{
+    class SegmentTree
+    {
+        private int[] _original, _tree, _lazy;
+        private int _size;
+        public SegmentTree(int[] a)
+        {
+            _size = (int)Math.Pow(2, Math.Ceiling(Math.Log(a.Length, 2))) * 2 - 1;
+            _original = a.ToArray();
+            _tree = Enumerable.Repeat(int.MaxValue, _size).ToArray();
+            _lazy = Enumerable.Repeat(0, _size).ToArray();
+            Construct(0, _original.Length - 1, 0);
+        }
+        private void Construct(int l, int r, int pos)
+        {
+            if (l == r) _tree[pos] = _original[l];
+            else
+            {
+                int mid = (l + r) / 2;
+                Construct(l, mid, pos * 2 + 1);
+                Construct(mid + 1, r, pos * 2 + 2);
+                _tree[pos] = Math.Min(_tree[pos * 2 + 1], _tree[pos * 2 + 2]);
+            }
+        }
+        public int QueryMin(int ql, int qr)
+        {
+            return QueryMin(ql, qr, 0, _original.Length - 1, 0);
+        }
+        private int QueryMin(int ql, int qr, int l, int r, int pos)
+        {
+            if (l > r) return int.MaxValue;
+
+            if (_lazy[pos] != 0)
+            {
+                _tree[pos] = _lazy[pos];
+                if (l != r)
+                {
+                    _lazy[pos * 2 + 1] = _lazy[pos];
+                    _lazy[pos * 2 + 2] = _lazy[pos];
+                }
+                _lazy[pos] = 0;
+            }
+
+            if (l >= ql && r <= qr) return _tree[pos];
+            else if (qr < l || ql > r) return int.MaxValue;
+            else
+            {
+                int mid = (l + r) / 2;
+                return Math.Min(QueryMin(ql, qr, l, mid, pos * 2 + 1), QueryMin(ql, qr, mid + 1, r, pos * 2 + 2));
+            }
+        }
+        public void Update(int ql, int qr, int value)
+        {
+            Update(ql, qr, 0, _original.Length - 1, 0, value);
+        }
+        private void Update(int ql, int qr, int l, int r, int pos, int value)
+        {
+            if (l > r) return;
+
+            if (_lazy[pos] != 0)
+            {
+                _tree[pos] = _lazy[pos];
+                if (l != r)
+                {
+                    _lazy[pos * 2 + 1] = _lazy[pos];
+                    _lazy[pos * 2 + 2] = _lazy[pos];
+                }
+                _lazy[pos] = 0;
+            }
+
+            if (l >= ql && r <= qr)
+            {
+                _tree[pos] = value;
+                if (l != r)
+                {
+                    _lazy[pos * 2 + 1] = value;
+                    _lazy[pos * 2 + 2] = value;
+                }
+            }
+            else if (qr < l || ql > r) return;
+            else
+            {
+                int mid = (l + r) / 2;
+                Update(ql, qr, l, mid, pos * 2 + 1, value);
+                Update(ql, qr, mid + 1, r, pos * 2 + 2, value);
+                _tree[pos] = Math.Min(_tree[pos * 2 + 1], _tree[pos * 2 + 1]);
+            }
+        }
+    }
+}
---
+++ CFTraining/CFTraining/Program.cs
@@ -0,0 +1,26 @@
+﻿using CFTraining.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 5, 4, 1, 3, 10, 7};
+            SegmentTree tree = new SegmentTree(a);
+            tree.Update(2, 4, 5);
+            tree.Update(3, 5, 9);
+            Console.WriteLine(tree.QueryMin(0, 5));
+            Console.WriteLine(tree.QueryMin(0, 2));
+            Console.WriteLine(tree.QueryMin(3, 5));
+            Console.WriteLine(tree.QueryMin(0, 1));
+            Console.WriteLine(tree.QueryMin(4, 5));
+            Console.WriteLine(tree.QueryMin(2, 2));
+        }
+    }
+}
