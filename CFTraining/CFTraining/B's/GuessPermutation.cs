3713125ff8e524a59c7f496b9dc26429c5c8855a
darkhan.zholmukhanov <dzholmukhanov@gmail.com>
2016-02-01 15:23:13 +0600

    Incomplete problems

---
+++ CFTraining/CFTraining/B's/GuessPermutation.cs
@@ -0,0 +1,75 @@
+﻿using CFTraining.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFTraining.B_s
+{
+    class GuessPermutation
+    {
+        // Not solved yet
+        public static void Run()
+        {
+            ConsoleScanner sc = new ConsoleScanner();
+            int n = sc.NextInt();
+            if (n == 2)
+            {
+                Console.ReadLine();
+                Console.ReadLine();
+                Console.WriteLine(2 + " " + 1);
+            }
+            else
+            {
+                int[,] a = new int[n, n];
+                int[] p = new int[n];
+                List<int> all = new List<int>();
+                for (int z = 0; z < n; z++)
+                {
+                    all.Add(z + 1);
+                    for (int w = 0; w < n; w++)
+                    {
+                        a[z, w] = sc.NextInt();
+                    }
+                }
+                int i, j, k, t, s, val = 0;
+                for (i = 0; i < n; i++)
+                {
+                    int oc = 0;
+                    for (j = 0; j < n; j++)
+                    {
+                        for (k = j + 1; k < n; k++)
+                        {
+                            if (a[i, j] == a[i, k])
+                            {
+                                s = i;
+                                t = j;
+                                oc++;
+                                val = a[i, j];
+                            }
+                        }
+                    }
+                    if (oc == 1) break;
+                }
+                for (int l = 0; l < n; l++)
+                {
+                    if (a[i, l] != 0 && a[i, l] != val)
+                    {
+                        p[l] = a[i, l];
+                        all.Remove(a[i, l]);
+                    }
+                }
+                for (int l = 0; l < n; l++)
+                {
+                    if (p[l] == 0)
+                    {
+                        p[l] = all.First();
+                        all.RemoveAt(0);
+                    }
+                    Console.Write(p[l] + " ");
+                }
+            }
+        }
+    }
+}
---
+++ CFTraining/CFTraining/C's/WetSharkFlowers341.cs
@@ -0,0 +1,51 @@
+﻿using CFTraining.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFTraining.C_s
+{
+    class WetSharkFlowers341
+    {
+        // Not solved yet
+        public static void Run()
+        {
+            ConsoleScanner sc = new ConsoleScanner();
+            int n = sc.NextInt(), p = sc.NextInt();
+            Shark[] s = new Shark[n];
+            long total = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int l = sc.NextInt(), r = sc.NextInt();
+                s[i] = new Shark { l = l, r = r};
+                total *= r - l + 1;
+            }
+            long num = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int l = i - 1, r = i + 1;
+                if (i == 0)
+                {
+                    l = n - 1;
+                }
+                else if (i == n - 1)
+                {
+                    r = 0;
+                }
+                long m1 = s[i].r / p - ((s[i].l - 1) / p), c1 = s[i].r - s[i].l + 1;
+                long m2 = s[l].r / p - ((s[l].l - 1) / p), c2 = s[l].r - s[l].l + 1;
+                long m3 = s[r].r / p - ((s[r].l - 1) / p), c3 = s[r].r - s[r].l + 1;
+                num += (m1 * c2 + m2 * (c1 - m1)) * (total / (c1 * c2));
+                num += (m1 * c3 + m3 * (c1 - m1)) * (total / (c1 * c3));
+            }
+            double res = (num * 1000.0) / (total * 1.0);
+            Console.WriteLine(res.ToString().Replace(",", "."));
+        }
+    }
+    class Shark
+    {
+        public int l, r;
+    }
+}
