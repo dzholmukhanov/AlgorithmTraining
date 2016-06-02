ed8eacd548d1bb2f184f485daa475500944c6404
dzholmukhanov@gmail.com <dzholmukhanov@gmail.com>
2016-06-03 00:36:29 +0600

    CFRound #353 C

--- AlgoTraining/AlgoTraining/Bootstrap.cs
+++ AlgoTraining/AlgoTraining/Bootstrap.cs
@@ -27,6 +27,7 @@ using AlgoTraining.Codeforces.B_s;
 using AlgoTraining.Codeforces.C_s;
 using AlgoTraining.Codeforces.A_s;
 using AlgoTraining.LightOJ;
+using AlgoTraining.Coursera;
 
 namespace CFTraining
 {
@@ -34,7 +35,7 @@ namespace CFTraining
     {
         public static void Main(string[] args)
         {
-            MadCounting.Run();
+            MaximumPairwiseMult.Run();
         }
     }
 }
---
+++ AlgoTraining/AlgoTraining/Codeforces/C's/MoneyTransfers353.cs
@@ -0,0 +1,34 @@
+﻿using CFTraining.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTraining.Codeforces.C_s
+{
+    class MoneyTransfers353
+    {
+        public static void Run()
+        {
+            using (FastScanner fs = new FastScanner(new BufferedStream(Console.OpenStandardInput())))
+            using (StreamWriter writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput())))
+            {
+                Dictionary<long, int> count = new Dictionary<long, int>();
+                int n = fs.NextInt();
+                int ans = n - 1;
+                long sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    int a = fs.NextInt();
+                    sum += a;
+                    if (!count.ContainsKey(sum)) count.Add(sum, 0);
+                    count[sum]++;
+                    ans = Math.Min(ans, n - count[sum]);
+                }
+                writer.WriteLine(ans);
+            }
+        }
+    }
+}
