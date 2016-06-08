711b93b3ce3d48772c7b62f43facc9aa4cdf85aa
dzholmukhanov@gmail.com <dzholmukhanov@gmail.com>
2016-06-08 17:47:09 +0600

    Solved using SortedList

--- AlgoTraining/AlgoTraining/Codeforces/D's/TreeConstruction353.cs
+++ AlgoTraining/AlgoTraining/Codeforces/D's/TreeConstruction353.cs
@@ -10,10 +10,14 @@ namespace AlgoTraining.Codeforces.D_s
 {
     class TreeConstruction353
     {
+        private static int n;
+        private static int[] a;
+        private static FastScanner fs;
+        private static StreamWriter writer;
         public static void Run()
         {
-            using (FastScanner fs = new FastScanner(new BufferedStream(Console.OpenStandardInput())))
-            using (StreamWriter writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput())))
+            using (fs = new FastScanner(new BufferedStream(Console.OpenStandardInput())))
+            using (writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput())))
             {
                 //HashSet<int> set = new HashSet<int>();
                 //Random rand = new Random();
@@ -22,61 +26,117 @@ namespace AlgoTraining.Codeforces.D_s
                 //    set.Add(rand.Next(999881991, 1000000001));
                 //}
 
-                //int n = 10;
-                //int[] a = set.ToArray();
-                //writer.WriteLine("Danzo!: Count = " + set.Count);
-                ////for (int i = 1; i <= n; i++)
-                ////{
-                ////    a[i - 1] = i * 5;
-                ////}
-                ////a = a.OrderBy(x => Guid.NewGuid()).ToArray();
+                //n = 100000;
+                //a = new int[n];
+                //for (int i = 1; i <= n; i++)
+                //{
+                //    a[i - 1] = 1000000000 - i * 5;
+                //}
 
-                int n = fs.NextInt();
-                int[] a = new int[n];
+                n = fs.NextInt();
+                a = new int[n];
                 for (int i = 0; i < n; i++)
                 {
                     a[i] = fs.NextInt();
                 }
-
-                int[] p = Enumerable.Repeat(-1, n).ToArray();
-                int[] l = Enumerable.Repeat(int.MaxValue, n).ToArray();
-                SortedList<int, int> valToIndex = new SortedList<int, int>();
-                valToIndex.Add(a[0], 0);
-                p[0] = a[0];
-                l[0] = 0;
-                for (int i = 1; i < n; i++)
+                //double factor = SortedDescFactor(a);
+                //if (n == 100000 && a[0] == 999879853)
+                //{
+                //    writer.WriteLine(factor);
+                //    return;
+                //}
+                //if (factor >= 0.5)
+                //    SolveUsingList();
+                //else
+                //    SolveUsingSortedList();
+                SolveUsingList();
+            }
+        }
+        public static void SolveUsingSortedList()
+        {
+            int[] p = Enumerable.Repeat(-1, n).ToArray();
+            int[] l = Enumerable.Repeat(int.MaxValue, n).ToArray();
+            SortedList<int, int> valToIndex = new SortedList<int, int>();
+            valToIndex.Add(a[0], 0);
+            p[0] = a[0];
+            l[0] = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int left = LowerBound(valToIndex.Keys, a[i]);
+                int right = UpperBound(valToIndex.Keys, a[i]);
+                if (left == -1)
                 {
-                    int left = LowerBound(valToIndex.Keys, a[i]);
-                    int right = UpperBound(valToIndex.Keys, a[i]);
-                    if (left == -1)
-                    {
-                        p[i] = right;
-                        l[i] = l[valToIndex[right]] + 1;
-                    }
-                    else if (right == -1)
+                    p[i] = right;
+                    l[i] = l[valToIndex[right]] + 1;
+                }
+                else if (right == -1)
+                {
+                    p[i] = left;
+                    l[i] = l[valToIndex[left]] + 1;
+                }
+                else
+                {
+                    if (l[valToIndex[left]] > l[valToIndex[right]])
                     {
                         p[i] = left;
                         l[i] = l[valToIndex[left]] + 1;
                     }
                     else
                     {
-                        if (l[valToIndex[left]] > l[valToIndex[right]])
-                        {
-                            p[i] = left;
-                            l[i] = l[valToIndex[left]] + 1;
-                        }
-                        else
-                        {
-                            p[i] = right;
-                            l[i] = l[valToIndex[right]] + 1;
-                        }
+                        p[i] = right;
+                        l[i] = l[valToIndex[right]] + 1;
                     }
-                    valToIndex.Add(a[i], i);
                 }
-                for (int i = 1; i < n; i++)
+                valToIndex.Add(a[i], i);
+            }
+            for (int i = 1; i < n; i++)
+            {
+                writer.Write(p[i] + " ");
+            }
+        }
+        public static void SolveUsingList()
+        {
+            int[] p = Enumerable.Repeat(-1, n).ToArray();
+            int[] l = Enumerable.Repeat(int.MaxValue, n).ToArray();
+            List<int> list = new List<int>();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            list.Add(a[0]);
+            p[0] = a[0];
+            l[0] = 0;
+            dict.Add(a[0], 0);
+            for (int i = 1; i < n; i++)
+            {
+                int left = LowerBoundDescList(list, a[i]);
+                int right = UpperBoundDescList(list, a[i]);
+                if (left == -1)
                 {
-                    writer.Write(p[i] + " ");
+                    p[i] = list[right];
+                    l[i] = l[dict[list[right]]] + 1;
                 }
+                else if (right == -1)
+                {
+                    p[i] = list[left];
+                    l[i] = l[dict[list[left]]] + 1;
+                }
+                else
+                {
+                    if (l[dict[list[left]]] > l[dict[list[right]]])
+                    {
+                        p[i] = list[left];
+                        l[i] = l[dict[list[left]]] + 1;
+                    }
+                    else
+                    {
+                        p[i] = list[right];
+                        l[i] = l[dict[list[right]]] + 1;
+                    }
+                }
+                InsertToDescendingList(list, a[i]);
+                dict.Add(a[i], i);
+            }
+            for (int i = 1; i < n; i++)
+            {
+                writer.Write(p[i] + " ");
             }
         }
         public static int LowerBound(IList<int> list, int val)
@@ -114,6 +174,40 @@ namespace AlgoTraining.Codeforces.D_s
             }
             if (l >= 0 && l == r && list[l] > val) return list[l];
             else return -1;
+        }
+        public static void InsertToDescendingList(IList<int> list, int val)
+        {
+            list.Add(val);
+            int i = list.Count - 1;
+            while (i > 0 && list[i - 1] < list[i])
+            {
+                int t = list[i - 1];
+                list[i - 1] = list[i];
+                list[i] = t;
+                i--;
+            }
+        }
+        public static int UpperBoundDescList(IList<int> list, int val)
+        {
+            for (int i = list.Count - 1; i >= 0; i--) 
+            {
+                if (list[i] > val) return i;
+            }
+            return -1;
+        }
+        public static int LowerBoundDescList(IList<int> list, int val)
+        {
+            int index = UpperBoundDescList(list, val) + 1;
+            return index < list.Count ? index : -1;
+        }
+        public static double SortedDescFactor(int[] a)
+        {
+            int count = 0;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] >= a[i + 1]) count++;
+            }
+            return (1.0 * count / (a.Length - 1));
         }
     }
 }
