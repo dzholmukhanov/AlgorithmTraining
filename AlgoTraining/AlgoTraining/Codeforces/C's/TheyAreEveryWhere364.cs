c5555ffe8d6fb5a78b95168b1c0beefb1d159649
dzholmukhanov@gmail.com <dzholmukhanov@gmail.com>
2016-07-31 23:22:13 +0600

    CF Round 364. Two pointers and Math.

---
+++ AlgoTraining/AlgoTraining/Codeforces/C's/TheyAreEveryWhere364.cs
@@ -0,0 +1,56 @@
+﻿using CFTraining.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTraining.Codeforces.C_s
+{
+    class TheyAreEveryWhere364
+    {
+        public static void Run()
+        {
+            using (FastScanner fs = new FastScanner(new BufferedStream(Console.OpenStandardInput())))
+            using (StreamWriter writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput())))
+            {
+                int n = fs.NextInt();
+                string s = fs.ReadLine();
+                int[] count = new int[52];
+                int total = 0;
+                foreach (char c in s)
+                {
+                    if (count[Index(c)] == 0) total++;
+                    count[Index(c)]++;
+                }
+                count = new int[52];
+                int k = 0, ans = int.MaxValue;
+                for (int l = 0, r = -1; l < n; l++)
+                {
+                    if (l > r)
+                    {
+                        r = l;
+                        if (count[Index(s[l])] == 0) k++;
+                        count[Index(s[l])]++;
+                    }
+                    while (k < total && r < n - 1)
+                    {
+                        r++;
+                        if (count[Index(s[r])] == 0) k++;
+                        count[Index(s[r])]++;
+                    }
+                    if (k == total) ans = Math.Min(ans, r - l + 1);
+                    else break;
+                    if (count[Index(s[l])] == 1) k--;
+                    count[Index(s[l])]--;
+                }
+                writer.WriteLine(ans);
+            }
+        }
+        public static int Index(char c) {
+            if (c >= 'a' && c <= 'z') return c - 'a';
+            else return c - 'A' + 26;
+        }
+    }
+}
---
+++ AlgoTraining/AlgoTraining/Codeforces/D's/AsFastAsPossible364.cs
@@ -0,0 +1,26 @@
+﻿using CFTraining.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoTraining.Codeforces.D_s
+{
+    class AsFastAsPossible364
+    {
+        public static void Run()
+        {
+            using (FastScanner fs = new FastScanner(new BufferedStream(Console.OpenStandardInput())))
+            using (StreamWriter writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput())))
+            {
+                int n = fs.NextInt(), l = fs.NextInt(), v1 = fs.NextInt(), v2 = fs.NextInt(), k = fs.NextInt();
+                int g = (int)Math.Ceiling(n * 1.0 / k);
+                double lbus = l * (1.0 * v1 + v2) / (v2 * 1.0 + (2 * g - 1) * v1);
+                double time = (l - lbus) / v1 + lbus / v2;
+                writer.WriteLine(time.ToString().Replace(",", "."));
+            }
+        }
+    }
+}
